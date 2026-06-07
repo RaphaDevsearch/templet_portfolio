b31ed4f0451e0a760ba80f0b6ebaf8b3cf64cc1d
RaphaDevsearch <mizaraphaelpro@gmail.com>
2026-06-07 22:52:26 +0530

    refactor Main component to generate paragraphs dynamically and update header/footer padding

--- components/Main.js
+++ components/Main.js
@@ -3,16 +3,14 @@ import {
 } from './Utils/CreateElement.js';
 
 export const Main = () => {
+  let nums = [];
+  for(let i = 1; i <= 100; i++){
+    nums.push(i);
+  }
   return CreateElement('main', {
     className: 'main',
-    children: [
-      CreateElement('h1', {
-        textContent: 'Welcome to My App'
-      }),
-
-      CreateElement('p', {
-        textContent: 'This is the main content area.'
-      })
-    ]
+    children: nums.map(num => CreateElement('p', {
+        textContent: `This is paragraph number ${num}`
+      }))
   });
 };
